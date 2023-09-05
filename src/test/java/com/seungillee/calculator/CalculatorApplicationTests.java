70d6e7cd4c73fc85188e72a14c17a5e846cb721a
seungil-lee <96454892+seungil-lee@users.noreply.github.com>
2023-09-05 22:46:05 +0900

    Add Spring Boot skeleton

---
+++ src/test/java/com/seungillee/calculator/CalculatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.seungillee.calculator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CalculatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
